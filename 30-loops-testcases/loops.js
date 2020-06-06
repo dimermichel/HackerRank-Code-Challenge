9eea110efd191b0d5f7984d58f6ed3332a4b97aa
Michel Maia <dimermichel@gmail.com>
2020-06-06 16:33:41 -0400

    Add Loops Challenge Solution

---
+++ 30-loops-testcases/loops.js
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/30-loops/problem
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function print(n){
+    for (let i = 1; i < 11 ; i++){
+        console.log(`${n} x ${i} = ${n * i}`)
+    }
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    print(n);
+}
